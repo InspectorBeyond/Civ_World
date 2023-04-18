060ee8dce376d8dc7a31fcb4bde336a8aa24e4a0
Inspector_Beyond <vitasgameslp@gmail.com>
2023-04-18 20:18:14 +0300

    Test1
    Test1

--- common/defines/civworld_defines.lua
+++ common/defines/civworld_defines.lua
@@ -4,7 +4,8 @@
 
 NDefines.NAI.COLONY_BUDGET_FRACTION = 0.9
 NDefines.NAI.DEFICIT_SPENDING_MIN_MONTHS = 6
-NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 2.0
+NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 1.0
 NDefines.NEconomy.MISSIONARY_MAINTENANCE_FACTOR = 1.0
-NDefines.NCountry.PROVINCE_DISCOVERY_YEARS_TECHNOLOGY = 1800		-- Years until discoveries spread within technology group
-NDefines.NCountry.PROVINCE_DISCOVERY_YEARS_RELIGION = 250		-- Years until discoveries spread with religion
+NDefines.NCountry.PROVINCE_DISCOVERY_YEARS_TECHNOLOGY = 189		-- Years until discoveries spread within technology group
+NDefines.NCountry.PROVINCE_DISCOVERY_YEARS_RELIGION = 189		-- Years until discoveries spread with religion
+NDefines.NAI.TRADE_POLICY_PROPAGATE_RELIGION_SCORE = 99.0				-- Score for propagate religion trade policy (if selectable)
